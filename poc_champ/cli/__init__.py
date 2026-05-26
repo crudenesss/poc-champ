718f500be83f613a1931605558dec718245be1a3
crudenesss <osbochok.w@gmail.com>
2026-05-26 23:35:22 +0300

    feat(cli): implement basic keyword and range inputs parsing

--- poc_champ/cli/__init__.py
+++ poc_champ/cli/__init__.py
@@ -1,2 +1,7 @@
+import argparse
+
 def get_parser():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--keyword", type=str)
+    parser.add_argument("-r", "--range", type=str)
+    return parser.parse_args()
