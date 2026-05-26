8629c880c5821223efd3f9ffc57fb6cf8dcf1be2
crudenesss <osbochok.w@gmail.com>
2026-05-26 23:24:19 +0300

    test(cli): add first basic test

---
+++ tests/test_input.py
@@ -0,0 +1,17 @@
+"""Tests for the input module."""
+
+import sys
+
+from poc_champ.cli import get_parser
+
+
+class TestInput:
+
+    """Test cases for the ArgParser input handling."""
+
+    def test_success_range_single_year(self, monkeypatch):
+        """Provide CVE year range limited to a single year."""
+        monkeypatch.setattr(sys, "argv", ["poc-champ", "-k", "keyword", "-r", "2026"])
+        args = get_parser()
+        assert args.keyword == "keyword"
+        assert args.range == "2026"
