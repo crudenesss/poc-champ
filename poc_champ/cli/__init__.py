d14962674dc006664d9a2406b8b4ab2a4e1da4a5
crudenesss <osbochok.w@gmail.com>
2026-05-27 15:41:18 +0300

    feat(cli): implement default range return if --range option is not provided

--- poc_champ/cli/__init__.py
+++ poc_champ/cli/__init__.py
@@ -2,12 +2,17 @@ import argparse
 import re
 import sys
 
+from datetime import datetime
+
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-k", "--keyword", type=str, required=True)
     parser.add_argument("-r", "--range", type=str)
     args = parser.parse_args()
-    if re.match(r"^\d{4}-\d{4}$", args.range):
+    if not args.range:
+        upper_year_threshold = datetime.now().year
+        args.range = [str(upper_year_threshold - 4), str(upper_year_threshold)]
+    elif re.match(r"^\d{4}-\d{4}$", args.range):
         args.range = args.range.split("-")
     elif re.match(r"^\d{4}$", args.range):
         pass
