efdab933ea0bc1e891fe50eb9b9a8f1da7959587
crudenesss <osbochok.w@gmail.com>
2026-05-27 18:16:37 +0300

    feat(cli): implement validation for provided cve

--- poc_champ/cli/parser.py
+++ poc_champ/cli/parser.py
@@ -16,7 +16,7 @@ class ConfigParser:
 
         group = self.parser.add_mutually_exclusive_group(required=True)
         group.add_argument("-k", "--keyword", type=str, help="Keywords to find related CVE's by.")
-        group.add_argument("-c", "--cve", type=str, help="CVE ID to find related POCs for.")
+        group.add_argument("-c", "--cve", type=self._validate_cve_format, help="CVE ID to find related POCs for.")
 
         self.parser.add_argument(
             "-r", "--range", type=self._validate_year_range, default=self._set_default_year_range()
@@ -38,6 +38,13 @@ class ConfigParser:
             sys.exit(1)
 
         return year_range
+
+    @staticmethod
+    def _validate_cve_format(cve):
+        if not re.match(r"^CVE-\d{4}-\d{4,}$", cve):
+            print(f"Argument {cve} is not a valid CVE format.")
+            sys.exit(1)
+        return cve
 
     def parse(self) -> argparse.Namespace:
         """Wrapper method around argparse internal processing. 
