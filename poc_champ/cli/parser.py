98b554c44ac75e96d9337aa697527bd7a3d7ebb8
crudenesss <osbochok.w@gmail.com>
2026-05-27 18:37:11 +0300

    refactor: move parser arguments setup into separate method

--- poc_champ/cli/parser.py
+++ poc_champ/cli/parser.py
@@ -13,13 +13,27 @@ class ConfigParser:
 
     def __init__(self, description):
         self.parser = argparse.ArgumentParser(description=description)
+        self._setup_arguments()
 
+    def _setup_arguments(self):
+        """Initialise CLI arguments."""
         group = self.parser.add_mutually_exclusive_group(required=True)
-        group.add_argument("-k", "--keyword", type=str, help="Keywords to find related CVE's by.")
-        group.add_argument("-c", "--cve", type=self._validate_cve_format, help="CVE ID to find related POCs for.")
+
+        group.add_argument(
+            "-k", "--keyword", 
+            type=str,
+            help="Keywords to find related CVE's by."
+        )
+        group.add_argument(
+            "-c", "--cve", 
+            type=self._validate_cve_format,
+            help="CVE ID to find related POCs for."
+        )
 
         self.parser.add_argument(
-            "-r", "--range", type=self._validate_year_range, default=self._set_default_year_range()
+            "-r", "--range", 
+            type=self._validate_year_range,
+            default=self._set_default_year_range()
         )
 
     @staticmethod
