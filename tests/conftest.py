7bfc3265a02fe70e72ada3a0d1bf4a168e845eb8
crudenesss <osbochok.w@gmail.com>
2026-05-29 01:11:35 +0300

    refactor(test): split tests into unit and integration

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from datetime import datetime
 DEFAULT_YEAR_COUNT = 5
 
 @pytest.fixture
-def excpected_default_year_range():
+def expected_default_year_range():
     """Fixture to provide the expected default year range for tests."""
     upper_year_threshold = datetime.now().year + 1
     return range(upper_year_threshold - DEFAULT_YEAR_COUNT, upper_year_threshold)
---
+++ tests/intergration/test_cli.py
@@ -0,0 +1,43 @@
+"""Integration tests for the cli system."""
+
+import sys
+import pytest
+
+from poc_champ.cli import get_parser
+
+
+class TestInput:
+
+    """Test cases for the unified cli input handling."""
+
+    def test_cli_success_range_single_year(self, monkeypatch):
+        """Provide CVE year range limited to a single year."""
+        monkeypatch.setattr(sys, "argv", ["poc-champ", "key", "keyword", "-r", "2026"])
+        args = get_parser()
+        assert args.keyword == "keyword"
+        assert args.range == range(2026, 2027)
+
+    def test_cli_success_range_year_range(self, monkeypatch):
+        """Provide CVE year range with a start and end year."""
+        monkeypatch.setattr(sys, "argv", ["poc-champ", "key", "keyword", "-r", "2020-2026"])
+        args = get_parser()
+        assert args.keyword == "keyword"
+        assert args.range == range(2020, 2027)
+
+    def test_cli_fail_invalid_year_range(self, monkeypatch):
+        """Provide an invalid year range where the start year is greater than the end year."""
+        monkeypatch.setattr(sys, "argv", ["poc-champ", "key", "keyword", "-r", "2020/2026"])
+        with pytest.raises(SystemExit):
+            get_parser()
+
+    def test_cli_success_cve(self, monkeypatch):
+        """Provide valid cve option."""
+        monkeypatch.setattr(sys, "argv", ["poc-champ", "cve", "CVE-2026-0001"])
+        args = get_parser()
+        assert args.cve_id == "CVE-2026-0001"
+
+    def test_cli_fail_invalid_cve_format(self, monkeypatch):
+        """Provide an invalid CVE format."""
+        monkeypatch.setattr(sys, "argv", ["poc-champ", "cve", "INVALID-CVE-FORMAT"])
+        with pytest.raises(SystemExit):
+            get_parser()
